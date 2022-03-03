c0de574aadd20238d3da32eb9dcd124a33414e1c
tom <23354981+tomfinney@users.noreply.github.com>
2022-03-03 15:02:25 +0000

    Changebtn (#1)
    * change btn to blue
    
    * adds changeset

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
 
 export const Button = () => {
-  return <button style={{ backgroundColor: "yellow" }}>Boop</button>;
+  return <button style={{ backgroundColor: "blue" }}>Boop</button>;
 };

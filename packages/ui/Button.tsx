3852511ab1bb62ddffb7eccb82a782c32ca71baa
James Page <23628247+james-page-dev@users.noreply.github.com>
2022-03-03 15:04:35 +0000

    feat: make button red

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,5 +1,3 @@
-import * as React from "react";
-
 export const Button = () => {
-  return <button style={{ backgroundColor: "blue" }}>Boop</button>;
+  return <button style={{ backgroundColor: "red" }}>Boop</button>;
 };
